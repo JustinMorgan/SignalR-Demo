7eedc609503619f4025f330ba073c42f64716c09
JustinMorgan <jmorgan@ncarb.org>
2014-07-03 14:54:33 -0400

    Initial Commit

---
+++ SignalR Demo/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalR_Demo.Startup))]
+namespace SignalR_Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
